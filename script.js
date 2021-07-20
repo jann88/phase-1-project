3a0915b81b4ebc65954b43f11d425df72732038e
Kyle Ortiz <kyleaortiz@gmail.com>
2021-07-20 17:42:57 -0500

    UI looks barely better, fetch it working! need to use data to create DOM elements now

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-function fetchWeather() {
+function fetchWeather(searchItem) {
   return fetch(
-    "http://api.weatherapi.com/v1/current.json?key=e245bd4daa254d44a24160310212007&q=Houston&aqi=yes"
+    `http://api.weatherapi.com/v1/current.json?key=e245bd4daa254d44a24160310212007&q=${searchItem}&aqi=yes`
   )
     .then((resp) => {
-      resp.json();
+      return resp.json();
     })
     .then((data) => {
-      console.log(data);
+      weatherLayout(data);
     });
 }
 
-fetchWeather();
+const button = document.getElementById("submit-button");
+button.addEventListener("click", buttonHandler);
+
+function buttonHandler() {
+  const inputValue = document.getElementById("weather-search").value;
+  fetchWeather(inputValue);
+}
+
+function weatherLayout(data) {
+  const rawData = data;
+}
