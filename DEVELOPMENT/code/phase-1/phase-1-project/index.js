e0786738e65e787b6bb27b0480daffcb56321896
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-06-24 08:03:38 +0300

    in progress

--- DEVELOPMENT/code/phase-1/phase-1-project/index.js
+++ DEVELOPMENT/code/phase-1/phase-1-project/index.js
@@ -6,3 +6,26 @@ fetch(apiData)
 function displayData(data) {
     document.querySelector("pre").innerHTML = JSON.stringify(data, null, 2);
 }
+let navbar = document.querySelector(".navbar");
+document.querySelector("#menu-btn").onclick = () => {
+    navbar.classlist.toggle("active");
+    searchForm.classlist.remove(active);
+    orderItem.classlist.remove(active);
+}
+let searchForm = document.querySelector("search-form");
+document.querySelector("#search-btn"), onclick = () => {
+    searchForm.classlist.toggle(active);
+    navbar.classlist.remove(active);
+    orderItem.classlist.remove(active);
+}
+let orderItem = document.querySelector("order-items-container");
+document.querySelector("#order-btn"), onclick = () => {
+    orderItem.classlist.toggle(active);
+    navbar.classlist.remove("active");
+    orderItem.classlist.remove(active);
+}
+window.onscroll = () => {
+    navbar.classlist.remove("active");
+    searchForm.classlist.remove(active);
+    orderItem.classlist.remove(active);
+}
