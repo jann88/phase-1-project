86093e8d30f192628f571992d28434a2bc677041
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-06-24 21:32:08 +0300

    working on it

--- DEVELOPMENT/code/phase-1/phase-1-project/index.js
+++ DEVELOPMENT/code/phase-1/phase-1-project/index.js
@@ -1,31 +1,223 @@
-const apiData = 'https://api.sampleapis.com/coffee/hot';
+const apiData = "https://rapidapi.com/Glavier/api/unlimited-google-search";
 fetch(apiData)
     .then(resp => resp.json())
-    .then(data => console.log(data[8].title.id));
+    .then(data => console.log(data));
 
 function displayData(data) {
     document.querySelector("pre").innerHTML = JSON.stringify(data, null, 2);
+
 }
-let navbar = document.querySelector(".navbar");
-document.querySelector("#menu-btn").onclick = () => {
-    navbar.classlist.toggle("active");
-    searchForm.classlist.remove(active);
-    orderItem.classlist.remove(active);
+
+/*let playlistApiData = 'https://watsonbox.github.io/exportify';
+fetch(playlistData)
+    .then(resp => resp.json())
+    .then(data => console.log(playlistData));
+
+
+function(data) {
+    document.querySelector("pre").innerHTML = JSON.stringify(data, null, 2);
+
+}*/
+let wordList = [{
+        word: "python",
+        hint: "programming language"
+    },
+    {
+        word: "guitar",
+        hint: "a musical instrument"
+    },
+    {
+        word: "aim",
+        hint: "a purpose or intention"
+    },
+    {
+        word: "venus",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "gold",
+        hint: "a yellow precious metal"
+    },
+    {
+        word: "ebay",
+        hint: "online shopping site"
+    },
+    {
+        word: "golang",
+        hint: "programming language"
+    },
+    {
+        word: "coding",
+        hint: "related to programming"
+    },
+    {
+        word: "matrix",
+        hint: "science fiction movie"
+    },
+    {
+        word: "bugs",
+        hint: "related to programming"
+    },
+    {
+        word: "avatar",
+        hint: "epic science fiction film"
+    },
+    {
+        word: "gif",
+        hint: "a file format for image"
+    },
+    {
+        word: "mental",
+        hint: "related to the mind"
+    },
+    {
+        word: "map",
+        hint: "diagram represent of an area"
+    },
+    {
+        word: "island",
+        hint: "land surrounded by water"
+    },
+    {
+        word: "hockey",
+        hint: "a famous outdoor game"
+    },
+    {
+        word: "chess",
+        hint: "related to a indoor game"
+    },
+    {
+        word: "viber",
+        hint: "a social media app"
+    },
+    {
+        word: "github",
+        hint: "code hosting platform"
+    },
+    {
+        word: "png",
+        hint: "a image file format"
+    },
+    {
+        word: "silver",
+        hint: "precious greyish-white metal"
+    },
+    {
+        word: "mobile",
+        hint: "an electronic device"
+    },
+    {
+        word: "gpu",
+        hint: "computer component"
+    },
+    {
+        word: "java",
+        hint: "programming language"
+    },
+    {
+        word: "google",
+        hint: "famous search engine"
+    },
+    {
+        word: "venice",
+        hint: "famous city of waters"
+    },
+    {
+        word: "excel",
+        hint: "microsoft product for windows"
+    },
+    {
+        word: "mysql",
+        hint: "a relational database system"
+    },
+    {
+        word: "nepal",
+        hint: "developing country name"
+    },
+    {
+        word: "flute",
+        hint: "a musical instrument"
+    },
+    {
+        word: "crypto",
+        hint: "related to cryptocurrency"
+    },
+    {
+        word: "tesla",
+        hint: "unit of magnetic flux density"
+    },
+    {
+        word: "mars",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "proxy",
+        hint: "related to server application"
+    },
+    {
+        word: "email",
+        hint: "related to exchanging message"
+    },
+    {
+        word: "html",
+        hint: "markup language for the web"
+    },
+    {
+        word: "air",
+        hint: "related to a gas"
+    },
+    {
+        word: "idea",
+        hint: "a thought or suggestion"
+    },
+    {
+        word: "server",
+        hint: "related to computer or system"
+    },
+    {
+        word: "svg",
+        hint: "a vector image format"
+    },
+    {
+        word: "jpeg",
+        hint: "a image file format"
+    },
+    {
+        word: "search",
+        hint: "act to find something"
+    },
+    {
+        word: "key",
+        hint: "small piece of metal"
+    },
+    {
+        word: "egypt",
+        hint: "a country name"
+    },
+    {
+        word: "joker",
+        hint: "psychological thriller film"
+    },
+    {
+        word: "dubai",
+        hint: "developed country name"
+    },
+    {
+        word: "photo",
+        hint: "representation of person or scene"
+    },
+    {
+        word: "nile",
+        hint: "largest river in the world"
+    },
+    {
+        word: "rain",
+        hint: "related to a water"
+    },
+]
+
+function randomWord() {
+    let ranObject = wordList[Math.floor(math.random() * wordList.length)];
+    console.log(ranObject);
 }
-let searchForm = document.querySelector("search-form");
-document.querySelector("#search-btn"), onclick = () => {
-    searchForm.classlist.toggle(active);
-    navbar.classlist.remove(active);
-    orderItem.classlist.remove(active);
-}
-let orderItem = document.querySelector("order-items-container");
-document.querySelector("#order-btn"), onclick = () => {
-    orderItem.classlist.toggle(active);
-    navbar.classlist.remove("active");
-    orderItem.classlist.remove(active);
-}
-window.onscroll = () => {
-    navbar.classlist.remove("active");
-    searchForm.classlist.remove(active);
-    orderItem.classlist.remove(active);
-}
+randomWord();
